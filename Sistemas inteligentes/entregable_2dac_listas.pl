2107c6e1d3e6e29f97bf37cb9663e4fb6d4f9d3d
Ricardo Muñoz Bolaños <ricardoucxiu@gmail.com>
2025-02-08 12:53:22 -0600

    El entregable de las listas todavía no esta listo jaja

--- Sistemas inteligentes/entregable_2dac_listas.pl
+++ Sistemas inteligentes/entregable_2dac_listas.pl
@@ -1,14 +1,16 @@
+%Acá arriba solamente se define una relación entre un objeto, y una lista de objetos.
 estudiante(juan,[fisica,algebra,espanol]).
 estudiante(rebeca,[fisica,algoritmica,quimica]).
 
-
+%Esto es una vil regurgitación.
 materias_est(Estudiante,Materias):-
 estudiante(Estudiante,Materias).
 
-inscrito(Estudiante,Materia):-
-estudiante(Estudiante,Materias),
-pertenece(Materia,Materias).
+%Ésta segunda línea ya es un tanto más decente
+inscrito(Estudiante,Materia):-%Un estudiante está inscrito en una materia, si…
+estudiante(Estudiante,Materias),%…existe una relación entre ese estudiante y una lista de materias…
+pertenece(Materia,Materias).%…en la que figura la materia en cuestión. 
 
 %Recursividad
-pertenece(X,[X|_]).%regla de parada (hmm, no lo creo, me suena a "!")
-pertenece(X,[_|T]):-pertenece(X,T).%Aquí se ejecuta primero pertenece pero con T siendo la lista que es la cola.
+pertenece(X,[X|_]).%Regla de parada (hmm, no lo creo, me suena a "!"). En lo general sí es una regla de parada. Pero en lo particular sólo el caracter "!" es regla de parada.
+pertenece(X,[_|T]):-pertenece(X,T).%Aquí se ejecuta primero pertenece pero con T siendo la lista que es la cola (es decir, aquí está la recursividad).
