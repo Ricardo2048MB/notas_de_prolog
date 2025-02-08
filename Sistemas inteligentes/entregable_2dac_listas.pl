0f4ef9777ecafe6146f6772c8ac96f9db2feb8eb
Ricardo Muñoz Bolaños <ricardoucxiu@gmail.com>
2025-02-08 15:27:26 -0600

    ¿Quiere unir 2 listas en el precio?
    Creo que ya entendí lo que quiere en 3 de los
    puntos de la tarea, pero la parte de los precios
    de los productos todavía está muy críptica.

--- Sistemas inteligentes/entregable_2dac_listas.pl
+++ Sistemas inteligentes/entregable_2dac_listas.pl
@@ -1,13 +1,19 @@
-%Acá arriba solamente se define una relación entre un objeto, y una lista de objetos.
+/*Acá arriba solamente se define una relación entre un objeto, y una 
+lista de objetos.*/
 estudia(juan,[fisica,algebra,espanol]).
 estudia(rebeca,[fisica,algoritmica,quimica]).
 
 
 %Ésta segunda línea ya es un tanto más decente
-inscrito(Estudiante,Materia):-%Se dice que un Estudiante está inscrito en un Materia, si…
-estudia(Estudiante,Materias),%…el Estudiante estudia los elementos de una lista Materias…
+inscrito(Estudiante,Materia):-/*Se dice que un Estudiante está inscrito en 
+una Materia, si…*/
+estudia(Estudiante,Materias),/*…el Estudiante estudia los elementos de una 
+lista Materias…*/
 pertenece(Materia,Materias).%…en la que justamente figura esa Materia.
 
 %Recursividad
-pertenece(X,[X|_]).%Regla de parada (hmm, no lo creo, me suena a "!"). En lo general sí es una regla de parada. Pero en lo particular sólo el caracter "!" es regla de parada.
-pertenece(X,[_|T]):-pertenece(X,T).%Aquí se ejecuta primero pertenece pero con T siendo la lista que es la cola (es decir, aquí está la recursividad).
+pertenece(X,[X|_]).%Regla de parada (hmm, no lo creo, me suena a "!"). 
+/*En lo general sí es una regla de parada. Pero en lo particular 
+sólo el caracter "!" es regla de parada.*/
+pertenece(X,[_|T]):-pertenece(X,T)./*Aquí se ejecuta primero pertenece pero 
+con T siendo la lista que es la cola (es decir, aquí está la recursividad).*/
