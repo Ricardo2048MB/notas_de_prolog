915d76bddb286a4a8fb1d38492f96bba75ed853a
Ricardo Muñoz Bolaños <ricardoucxiu@gmail.com>
2025-02-08 23:07:51 -0600

    Formatee un poco mi archivo de la Tienda

--- Sistemas inteligentes/Tienda de electrónica.pl
+++ Sistemas inteligentes/Tienda de electrónica.pl
@@ -29,11 +29,57 @@ pertenece(X,[_|T]):-pertenece(X,T).%Aquí se ejecuta primero pertenece pero
 %con T siendo la lista que es la cola (es decir, aquí está la recursividad).
 
 
-compatibilidad(['RAM DDR4 32 GB Hello Kitty','Reloj RGB Casio','Ventiladores de enfriamiento snowman','USB HUB 4 puertos Pulptastic','Lector multi-tarjetas Rockman','Cable HDMI trenzado Premium 8K Eyesight','Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Linkman','Panel de botones de superposición gráfica Anchor'],'Computadora gamer ASUS ROG').
-compatibilidad(['RAM DDR4 32 GB Halo','Reloj RGB Ferrari','Ventiladores de enfriamiento Olaf','USB HUB 4 puertos YameteKudasai','Lector multi-tarjetas Ozzyman','Cable HDMI trenzado Premium 8K IC2','Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Oh-my-link','Panel de botones de superposición gráfica Anchovy'],'Computadora gamer Alienware').
-compatibilidad(['RAM DDR4 32 GB Corsair','Reloj RGB Senheiser','Ventiladores de enfriamiento Elsational','USB HUB 4 puertos Hubington','Lector multi-tarjetas AWL','Cable HDMI trenzado Premium 8K Blindshot','Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Enocman','Panel de botones de superposición gráfica Vault-tec'],'Computadora gamer Acer Predator').
-compatibilidad(['RAM DDR4 32 GB Steelers','Reloj RGB Holcim','Ventiladores de enfriamiento Mr. Freeze','USB HUB 4 puertos Spicy Horse','Lector multi-tarjetas 4x4','Cable HDMI trenzado Premium 8K AIWA','Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Xenomorph','Panel de botones de superposición gráfica Anchor premium'],'Computadora gamer Lenovo Legion').
-compatibilidad(['RAM DDR4 32 GB Avengers','Reloj RGB Alnitak','Ventiladores de enfriamiento Dr. Snowflake','USB HUB 4 puertos Havok','Lector multi-tarjetas Wildboar','Cable HDMI trenzado Premium 8K Oleico','Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Thunderbolt','Panel de botones de superposición gráfica Holland'],'Computadora gamer HP Omen').
+compatibilidad(['RAM DDR4 32 GB Hello Kitty',
+				'Reloj RGB Casio','Ventiladores de enfriamiento snowman',
+				'USB HUB 4 puertos Pulptastic',
+				'Lector multi-tarjetas Rockman',
+				'Cable HDMI trenzado Premium 8K Eyesight',
+				'Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Linkman',
+				'Panel de botones de superposición gráfica Anchor'],
+				'Computadora gamer ASUS ROG').
+
+
+compatibilidad(['RAM DDR4 32 GB Halo',
+				'Reloj RGB Ferrari',
+				'Ventiladores de enfriamiento Olaf',
+				'USB HUB 4 puertos YameteKudasai',
+				'Lector multi-tarjetas Ozzyman',
+				'Cable HDMI trenzado Premium 8K IC2',
+				'Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Oh-my-link',
+				'Panel de botones de superposición gráfica Anchovy'],
+				'Computadora gamer Alienware').
+
+
+compatibilidad(['RAM DDR4 32 GB Corsair','Reloj RGB Senheiser',
+				'Ventiladores de enfriamiento Elsational',
+				'USB HUB 4 puertos Hubington',
+				'Lector multi-tarjetas AWL',
+				'Cable HDMI trenzado Premium 8K Blindshot',
+				'Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Enocman',
+				'Panel de botones de superposición gráfica Vault-tec'],
+				'Computadora gamer Acer Predator').
+
+
+compatibilidad(['RAM DDR4 32 GB Steelers',
+				'Reloj RGB Holcim',
+				'Ventiladores de enfriamiento Mr. Freeze',
+				'USB HUB 4 puertos Spicy Horse',
+				'Lector multi-tarjetas 4x4',
+				'Cable HDMI trenzado Premium 8K AIWA',
+				'Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Xenomorph',
+				'Panel de botones de superposición gráfica Anchor premium'],
+				'Computadora gamer Lenovo Legion').
+
+
+compatibilidad(['RAM DDR4 32 GB Avengers',
+				'Reloj RGB Alnitak',
+				'Ventiladores de enfriamiento Dr. Snowflake',
+				'USB HUB 4 puertos Havok',
+				'Lector multi-tarjetas Wildboar',
+				'Cable HDMI trenzado Premium 8K Oleico',
+				'Cable de fibra óptica 10g Transmisión Pd60w Tipo-C a C Thunderbolt',
+				'Panel de botones de superposición gráfica Holland'],
+				'Computadora gamer HP Omen').
 
 
 compatible(Accesorio,Producto):-
@@ -41,11 +87,56 @@ compatibilidad(Accesorios,Producto),
 pertenece(Accesorio,Accesorios).
 
 
-colores_disponibles(['Rojo','Naranja','Amarillo','Verde','Azul','Añil mendicante','Violeta','Gris metálico'],'Computadora gamer HP Omen').
-colores_disponibles(['Rojo infernal','Naranja Hambriento','Amarillo Cegador','Verde espectral','Azul petrificante','Añil mate','Violeta venomous','Gris metálico'],'Computadora gamer Lenovo Legion').
-colores_disponibles(['Rojo furioso','Naranja voraz','Amarillo amenazante','Verde camuflaje','Azul agilidad','Añil letalidad','Violeta intempestivo','Gris metálico'],'Computadora gamer Acer Predator').
-colores_disponibles(['Rojo invasión','Naranja aniquilador','Amarillo 51','Verde Marciano','Azul plasmático','Añil clonación','Violeta ultratecnológico','Gris metálico'],'Computadora gamer Alienware').
-colores_disponibles(['Rojo Competitivo','Naranja Ambicioso','Amarillo Esperanzado','Verde Explorador','Azul Eficiente','Añil Manco','Violeta E-girl','Gris metálico'],'Computadora gamer ASUS ROG').
+colores_disponibles(['Rojo','Naranja',
+					'Amarillo','Verde','Azul',
+					'Añil mendicante',
+					'Violeta',
+					'Gris metálico'],
+					'Computadora gamer HP Omen').
+
+
+colores_disponibles(['Rojo infernal',
+					'Naranja Hambriento',
+					'Amarillo Cegador',
+					'Verde espectral',
+					'Azul petrificante',
+					'Añil mate',
+					'Violeta venomous',
+					'Gris metálico'],
+					'Computadora gamer Lenovo Legion').
+
+
+colores_disponibles(['Rojo furioso',
+					'Naranja voraz',
+					'Amarillo amenazante',
+					'Verde camuflaje',
+					'Azul agilidad',
+					'Añil letalidad',
+					'Violeta intempestivo',
+					'Gris metálico'],
+					'Computadora gamer Acer Predator').
+
+
+colores_disponibles(['Rojo invasión',
+					'Naranja aniquilador',
+					'Amarillo 51',
+					'Verde Marciano',
+					'Azul plasmático',
+					'Añil clonación',
+					'Violeta ultratecnológico',
+					'Gris metálico'],
+					'Computadora gamer Alienware').
+
+
+colores_disponibles(['Rojo Competitivo',
+					'Naranja Ambicioso',
+					'Amarillo Esperanzado',
+					'Verde Explorador',
+					'Azul Eficiente',
+					'Añil Manco',
+					'Violeta E-girl',
+					'Gris metálico'],
+					'Computadora gamer ASUS ROG').
 
 
 color_disponible(Color,Producto):-
