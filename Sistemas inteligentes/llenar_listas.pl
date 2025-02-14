8021991b7a0a6604864640496a8ac1df724c56cf
Ricardo2048MB <rkscpvf@gmail.com>
2025-02-14 15:37:45 -0600

    Listo, ya tengo la tarea, pero me costó un huevo

--- Sistemas inteligentes/llenar_listas.pl
+++ Sistemas inteligentes/llenar_listas.pl
@@ -1,3 +1,9 @@
+%%%%%%%%%%%%%%%%%%%%%%%%Esta cosa incluye elementos específicos (espero, jaja)
+agregar_especificos([_e0,_e3,_,_e5|_],[_e6,_e8|_],[_e1,_e4|_],[_e7,_e9|_],[_e2|_],Toloquesobra,Resultado):-
+    ListaAuxiliar = [_e0,_e1,_e2,_e3,_e4,_e5,_e6,_e7,_e8,_e9],
+    append(ListaAuxiliar,Toloquesobra,Resultado).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%Llenar lista (general)
 llenar_lista(Lista):-
     writeln('Escribe un dato (escriba "fin." para terminar de llenar esta lista)'),
     read(Dato),
@@ -58,6 +64,7 @@ iniciar:-
     writeln('Imprimiendo lista de marcas de autos...'),
     writeln(ListaMarcasAutos),
 
+%%%%%%%%%%%%%%%%%%%%%%%%Hasta este momento yo no sabía como appendizar bien jaja
     append(ListaNumeros,ListaColoresES,ListaTotal1),
     append(ListaMayusculas,ListaColoresEN,ListaTotal2),
     append(ListaXYZF,ListaMarcasAutos,ListaTotal3),
@@ -65,11 +72,27 @@ iniciar:-
     append(ListaTotal1,ListaTotal2,ListaTotal1y2),
     append(ListaTotal1y2,ListaTotal3,ListaTotal12y3),
 
+/* Siento que aquí pude haber hecho una cadenita en lugar de muchas ListasTotales,
+pero bueno ya ni modos, yo he fallado, como dijo MC Dinero. */
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     writeln('Imprimiendo lista de todo...'),
-    writeln(ListaTotal12y3).
+    writeln(ListaTotal12y3),
 
 
+    %%%%%%ACÁ
+    agregar_especificos(
+        ListaNumeros,
+        ListaColoresES,
+        ListaMayusculas,
+        ListaColoresEN,
+        ListaXYZF,
+        ListaMarcasAutos,
+        ListaPizarron),
+
+    writeln('Imprimiendo lista como en el pizarrón...'),
+    writeln(ListaPizarron).
+    %%%%%%%%ESTO ERA EL CÓDIGO QUE HABÍAMOS HECHO EN CLASE
     /* writeln('Llenar datos a las listas...'),
     writeln('Llenar datos a la lista 1:'),
     llenar_lista(ListaDatos),
@@ -85,7 +108,8 @@ iniciar:-
     writeln(ListaFinal). */
 
 /*
-    Llenar 6 listas:
+    De tarea:
+    Llenar éstas 6 listas:
 
     1.- Números
     2.- Colores en Español
